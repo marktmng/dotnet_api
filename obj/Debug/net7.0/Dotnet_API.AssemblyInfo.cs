7d2ab3552901e85ef87651c0ce96581c5a7021b5
Mark Tmng <mailsareaccepting@gmail.com>
2024-11-09 14:14:06 +1300

    don't know what is it

--- obj/Debug/net7.0/Dotnet_API.AssemblyInfo.cs
+++ obj/Debug/net7.0/Dotnet_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12bd59b45f3b2354be468fa25838fd66afef7ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84583c6422375dd031ee5f016cd3ca420a7c7e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
