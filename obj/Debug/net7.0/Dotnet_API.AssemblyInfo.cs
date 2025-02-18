63979575bdfb541235c4a5d7f56647090bbb0c2c
Mark Tmng <mailsareaccepting@gmail.com>
2025-02-18 23:24:26 +1300

    fixing

--- obj/Debug/net7.0/Dotnet_API.AssemblyInfo.cs
+++ obj/Debug/net7.0/Dotnet_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915cdbb976b7cfeaa978dac4b87a4756084c4e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f3683d2a566be28242e85d60beb51451ff78c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
