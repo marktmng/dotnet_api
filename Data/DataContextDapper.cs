81f3683d2a566be28242e85d60beb51451ff78c3
Mark Tmng <mailsareaccepting@gmail.com>
2025-02-18 23:19:15 +1300

    trying to fix

--- Data/DataContextDapper.cs
+++ Data/DataContextDapper.cs
@@ -38,3 +38,5 @@ namespace DotnetAPI
         }
     }
 }
+
+// trying to fix
--- obj/Debug/net7.0/Dotnet_API.AssemblyInfo.cs
+++ obj/Debug/net7.0/Dotnet_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9196adea253fb0eb26d3efbc39be8b6dc3c66e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915cdbb976b7cfeaa978dac4b87a4756084c4e97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dotnet_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
